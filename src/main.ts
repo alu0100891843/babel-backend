fcca73df8a9639f91947174b849c94989c7f2672
Cristian Manuel Ángel Díaz <alu0100891843@ull.edu.es>
2025-06-12 14:24:15 +0100

    Create handleError middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HandleErrorMiddleware } from './common/middlewares/handleError.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN || 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+
+  app.useGlobalFilters(new HandleErrorMiddleware());
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((error) => console.error(error));
